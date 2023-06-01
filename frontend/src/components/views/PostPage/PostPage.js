772e1576f23ec19f7da2218842fb0f91f93091e6
Priya Chaudhary <pri8394chaudhary@gmail.com>
2023-06-01 18:17:21 +0530

    first commit

--- frontend/src/components/views/PostPage/PostPage.js
+++ frontend/src/components/views/PostPage/PostPage.js
@@ -1,54 +1,54 @@
-// import React, { useEffect, useState } from 'react'
-// import axios from 'axios';
-// import { Typography } from 'antd';
-// import Navbar from '../../Navbar/Navbar';
-// import Footer from '../../Footer/Footer';
-// const { Title } = Typography
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Typography } from 'antd';
+import Navbar from '../../Navbar/Navbar';
+import Footer from '../../Footer/Footer';
+const { Title } = Typography
 
-// function PostPage(props) {
+function PostPage(props) {
 
-//     const [post, setPost] = useState([])
-//     const postId = props.match.params.postId;
+    const [post, setPost] = useState([])
+    const postId = props.match.params.postId;
 
-//     useEffect(() => {
+    useEffect(() => {
 
-//         const variable = { postId: postId }
+        const variable = { postId: postId }
 
-//         axios.post('http://localhost:2000/api/getPost',variable, {
-//             headers:{
-//                 'auth-token':localStorage.getItem('token')
-//             } 
-//         })
-//             .then(response => {
-//                 if (response.data.success) {
-//                     console.log(response.data.post)
-//                     setPost(response.data.post)
-//                 } else {
-//                     alert('Couldnt get post')
-//                 }
-//             })
-//     }, [])
+        axios.post('http://localhost:2000/api/getPost',variable, {
+            headers:{
+                'auth-token':localStorage.getItem('token')
+            } 
+        })
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data.post)
+                    setPost(response.data.post)
+                } else {
+                    alert('Couldnt get post')
+                }
+            })
+    }, [])
 
    
-//         return (
-//             <>
-//             <Navbar/>
-//             <div className="postPage" style={{ width: '80%', margin: '3rem auto' }}>
+        return (
+            <>
+            <Navbar/>
+            <div className="postPage" style={{ width: '80%', margin: '3rem auto' }}>
                
-//                 <br />
-//                 <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-//                     <Title style={{letterSpacing:"2px"}} level={4}>{post.createdAt}</Title>
-//                 </div>
-//                 <h2 style={{textAlign:"center"}}><b>{post.chapter}</b></h2><hr/>
-//                 <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                <br />
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Title style={{letterSpacing:"2px"}} level={4}>{post.createdAt}</Title>
+                </div>
+                <h2 style={{textAlign:"center"}}><b>{post.chapter}</b></h2><hr/>
+                <div dangerouslySetInnerHTML={{ __html: post.content }} />
 
-//             </div>
-//             <Footer/>
-//             </>
-//         )
+            </div>
+            <Footer/>
+            </>
+        )
     
     
 
-// }
+}
 
-// export default PostPage
+export default PostPage
